c795a24d62a25dc09108cb9b85928a2a5e050632
wuxinkui <wuxinkui@git>
2023-12-13 18:01:17 +0800

    feat: test redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
+import {createClient} from "redis";
 
 @Module({
-  imports: [PersonModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: 'REDIS_CLIENT',
+      async useFactory() {
+        const client = createClient({
+            socket: {
+              host: 'localhost',
+              port: 6379
+            }
+        }
+        )
+        await client.connect()
+        return client
+      }
+    }],
 })
 export class AppModule {}
